1e46128f33b0c1dc94729c4826764633435419ec
vadim8960 <akimovvadim779@gmail.com>
2020-04-02 22:58:24 +0300

    Complete task #4.

--- 4ПРИМ_УмножениеМатрицыНаВекор/Vadim.dfm
+++ 4ПРИМ_УмножениеМатрицыНаВекор/Vadim.dfm
@@ -1,8 +1,8 @@
 object Valerievich: TValerievich
   Left = 192
   Top = 125
-  Width = 722
-  Height = 419
+  Width = 726
+  Height = 661
   Caption = #1059#1084#1085#1086#1078#1077#1085#1080#1077' '#1084#1072#1090#1088#1080#1094#1099' '#1085#1072' '#1074#1077#1082#1090#1086#1088
   Color = clBtnFace
   Font.Charset = RUSSIAN_CHARSET
@@ -20,6 +20,13 @@ object Valerievich: TValerievich
     Width = 179
     Height = 21
     Caption = #1056#1072#1079#1084#1077#1088#1085#1086#1089#1090#1100' '#1084#1072#1090#1088#1080#1094#1099
+  end
+  object Label2: TLabel
+    Left = 40
+    Top = 416
+    Width = 180
+    Height = 21
+    Caption = #1047#1085#1072#1095#1077#1085#1080#1103' '#1076#1083#1103' '#1087#1088#1080#1084#1077#1088#1072
   end
   object Edit1: TEdit
     Left = 208
@@ -82,5 +89,45 @@ object Valerievich: TValerievich
       24
       24
       25)
+  end
+  object Button3: TButton
+    Left = 264
+    Top = 376
+    Width = 217
+    Height = 25
+    Caption = #1055#1086#1082#1072#1079#1072#1090#1100' '#1076#1086#1087'. '#1085#1072#1089#1090#1088#1086#1081#1082#1080
+    TabOrder = 6
+    OnClick = Button3Click
+  end
+  object RadioGroup1: TRadioGroup
+    Left = 16
+    Top = 448
+    Width = 241
+    Height = 153
+    Caption = #1056#1072#1079#1084#1077#1088' '#1084#1072#1090#1088#1080#1094#1099
+    Items.Strings = (
+      '3 '#1093' 3'
+      '4 '#1093' 4'
+      '5 '#1093' 5')
+    TabOrder = 7
+    OnClick = RadioGroup1Click
+  end
+  object Button4: TButton
+    Left = 288
+    Top = 480
+    Width = 265
+    Height = 25
+    Caption = #1043#1077#1085#1077#1088#1072#1094#1080#1103' '#1079#1085#1072#1095#1077#1085#1080#1081' '#1086#1090' -3 '#1076#1086' 3'
+    TabOrder = 8
+    OnClick = Button4Click
+  end
+  object Button5: TButton
+    Left = 288
+    Top = 520
+    Width = 265
+    Height = 25
+    Caption = #1054#1090#1095#1080#1089#1090#1080#1090#1100' '#1074#1089#1077
+    TabOrder = 9
+    OnClick = Button5Click
   end
 end
--- 4ПРИМ_УмножениеМатрицыНаВекор/Vadim.pas
+++ 4ПРИМ_УмножениеМатрицыНаВекор/Vadim.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Grids, StdCtrls;
+  Dialogs, Grids, StdCtrls, ExtCtrls;
 
 type
   TValerievich = class(TForm)
@@ -15,35 +15,106 @@ type
     Button2: TButton;
     StringGrid2: TStringGrid;
     StringGrid3: TStringGrid;
+    Button3: TButton;
+    RadioGroup1: TRadioGroup;
+    Label2: TLabel;
+    Button4: TButton;
+    Button5: TButton;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure RadioGroup1Click(Sender: TObject);
   private
-    { Private declarations }
+    size: Integer;
+    flag_button_pressed: Boolean;
+    procedure ClearTables();
+    procedure ResizeGrid(size_tmp: Integer);
+    procedure ReadDataFromFile(size_tmp: Integer; path: String);
+    procedure ShowAdvanced();
+    procedure HideAdvanced();
   public
     { Public declarations }
   end;
 
 var
   Valerievich: TValerievich;
-  size: Integer;
 
 implementation
 
 {$R *.dfm}
 
-procedure ResizeGrid(size: Integer);
+procedure TValerievich.ClearTables();
+var
+  i, j: Integer;
+begin
+  for i := 1 to size do begin
+    for j := 1 to size do begin
+      StringGrid1.Cells[i, j] := '';
+    end;
+    StringGrid2.Cells[0, i] := '';
+    StringGrid3.Cells[0, i] := '';
+  end;
+end;
+
+procedure TValerievich.ResizeGrid(size_tmp: Integer);
 var
   i: Integer;
 begin
-  Valerievich.StringGrid1.ColCount := size + 1;
-  Valerievich.StringGrid1.RowCount := size + 1;
+  size := size_tmp;
+  StringGrid1.ColCount := size + 1;
+  StringGrid1.RowCount := size + 1;
   for i := 0 to size do begin
-    Valerievich.StringGrid1.Cells[0, i + 1] := 'b' + IntToStr(i);
-    Valerievich.StringGrid1.Cells[i + 1, 0] := 'a' + IntToStr(i);
+    StringGrid1.Cells[0, i + 1] := 'b' + IntToStr(i);
+    StringGrid1.Cells[i + 1, 0] := 'a' + IntToStr(i);
   end;
-  Valerievich.StringGrid2.RowCount := size + 1;
-  Valerievich.StringGrid3.RowCount := size + 1;
+  StringGrid2.RowCount := size + 1;
+  StringGrid3.RowCount := size + 1;
+  ClearTables();
+end;
+
+procedure TValerievich.ReadDataFromFile(size_tmp: Integer; path: String);
+var
+  file_v: TextFile;
+  value: real;
+  i, j: Integer;
+begin
+  if (FileExists(path)) then begin
+    ResizeGrid(size_tmp);
+    AssignFile(file_v, path);
+    Reset(file_v);
+    for i := 1 to size do begin
+      for j := 1 to size do begin
+        Read(file_v, value);
+        StringGrid1.Cells[i, j] := FloatToStrF(value, fffixed, 5, 8);
+      end;
+    end;
+    for i := 1 to size do begin
+      Read(file_v, value);
+      StringGrid2.Cells[0, i] := FloatToStrF(value, fffixed, 5, 8);
+    end;
+    CloseFile(file_v);
+  end else begin
+    ShowMessage('Файл не найден. Проверьте его существование.');
+  end;
+end;
+
+procedure TValerievich.ShowAdvanced();
+begin
+  RadioGroup1.Show;
+  Label2.Show;
+  Button4.Show;
+  Button5.Show;
+end;
+
+procedure TValerievich.HideAdvanced();
+begin
+  RadioGroup1.Hide;
+  Label2.Hide;
+  Button4.Hide;
+  Button5.Hide;
 end;
 
 procedure TValerievich.Button1Click(Sender: TObject);
@@ -59,9 +130,11 @@ end;
 
 procedure TValerievich.FormCreate(Sender: TObject);
 var
-  i, j: Integer;
+  i, j, begin_size: Integer;
 begin
-  size := 3;
+  begin_size := 3;
+  ResizeGrid(begin_size);
+
   StringGrid1.Cells[0, 0] := 'Матрица A';
   StringGrid1.ColWidths[0] := 95;
   StringGrid1.FixedCols := 1;
@@ -76,8 +149,6 @@ begin
   StringGrid3.ColWidths[0] := 95;
   StringGrid3.FixedCols := 0;
   StringGrid3.FixedRows := 1;
-  
-  ResizeGrid(size);
 
   for i := 1 to size do begin
     for j := 1 to size do begin
@@ -85,6 +156,11 @@ begin
     end;
     StringGrid2.Cells[0, i] := FloatToStrF(i, fffixed, 8, 5)
   end;
+  
+  flag_button_pressed := False;
+  Valerievich.Height := 456;
+  Button3.Caption := 'Показать доп. настройки';
+  HideAdvanced();
 end;
 
 procedure TValerievich.Button2Click(Sender: TObject);
@@ -113,6 +189,49 @@ begin
     end;
     StringGrid3.Cells[0, i] := FloatToStrF(sum, fffixed, 8, 5);
   end;
+end;
+
+procedure TValerievich.Button3Click(Sender: TObject);
+begin
+  if (flag_button_pressed) then begin
+    flag_button_pressed := False;
+    Valerievich.Height := 456;
+    Button3.Caption := 'Показать доп. настройки';
+    HideAdvanced();
+  end else begin
+    flag_button_pressed := True;
+    Valerievich.Height := 661;
+    Button3.Caption := 'Скрыть доп. настройки';
+    ShowAdvanced();
+  end;
+end;
+
+procedure TValerievich.Button5Click(Sender: TObject);
+begin
+  ClearTables();
+end;
+
+procedure TValerievich.Button4Click(Sender: TObject);
+var
+  i, j: Integer;
+begin
+  Randomize;
+  for i := 1 to size do begin
+    for j := 1 to size do begin
+      StringGrid1.Cells[i, j] := FloatToStrF(3 - Random * 6, fffixed, 5, 8);
+    end;
+    StringGrid2.Cells[0, i] := FloatToStrF(3 - Random * 6, fffixed, 5, 8);
+  end;
+end;
+
+procedure TValerievich.RadioGroup1Click(Sender: TObject);
+begin
+  case RadioGroup1.ItemIndex of
+    0: ReadDataFromFile(3, '3x3.txt');
+    1: ReadDataFromFile(4, '4x4.txt');
+    2: ReadDataFromFile(5, '5x5.txt');
+  end;
+  RadioGroup1.ItemIndex := -1;
 end;
 
 end.
