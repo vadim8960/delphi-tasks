b080ec74d6c0e2d22db9f3bf3f26f36a899371ec
vadim8960 <akimovvadim779@gmail.com>
2020-05-24 17:20:22 +0300

    Complete task #6.4

---
+++ 6ПРИМ_4ВедомостьАбитуриентов/Vadim.dfm
@@ -0,0 +1,158 @@
+object Valerievich: TValerievich
+  Left = 168
+  Top = 122
+  Width = 797
+  Height = 393
+  Caption = #1042#1077#1076#1086#1084#1086#1089#1090#1100' '#1072#1073#1080#1090#1091#1088#1080#1077#1090#1085#1086#1074
+  Color = clBtnFace
+  Font.Charset = RUSSIAN_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -19
+  Font.Name = 'Times New Roman'
+  Font.Style = []
+  Menu = MainMenu1
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 21
+  object Label1: TLabel
+    Left = 16
+    Top = 16
+    Width = 56
+    Height = 21
+    Caption = #1060'.'#1048'.'#1054'.'
+  end
+  object edtName: TEdit
+    Left = 16
+    Top = 48
+    Width = 209
+    Height = 29
+    TabOrder = 0
+    Text = 'edtName'
+  end
+  object GroupBox1: TGroupBox
+    Left = 16
+    Top = 96
+    Width = 209
+    Height = 177
+    Caption = #1054#1094#1077#1085#1082#1080
+    TabOrder = 1
+    object Label2: TLabel
+      Left = 8
+      Top = 40
+      Width = 95
+      Height = 21
+      Caption = #1052#1072#1090#1077#1084#1072#1090#1080#1082#1072
+    end
+    object Label3: TLabel
+      Left = 8
+      Top = 88
+      Width = 59
+      Height = 21
+      Caption = #1060#1080#1079#1080#1082#1072
+    end
+    object Label4: TLabel
+      Left = 8
+      Top = 136
+      Width = 89
+      Height = 21
+      Caption = #1057#1086#1095#1080#1085#1077#1085#1080#1077
+    end
+    object edtMath: TEdit
+      Left = 128
+      Top = 40
+      Width = 49
+      Height = 29
+      TabOrder = 0
+      Text = 'edtMath'
+    end
+    object edtPhis: TEdit
+      Left = 128
+      Top = 88
+      Width = 49
+      Height = 29
+      TabOrder = 1
+      Text = 'edtPhis'
+    end
+    object edtLetter: TEdit
+      Left = 128
+      Top = 136
+      Width = 49
+      Height = 29
+      TabOrder = 2
+      Text = 'edtLetter'
+    end
+  end
+  object btnCreate: TButton
+    Left = 16
+    Top = 288
+    Width = 209
+    Height = 33
+    Caption = 'btnCreate'
+    TabOrder = 2
+    OnClick = btnCreateClick
+  end
+  object sgrdTable: TStringGrid
+    Left = 248
+    Top = 48
+    Width = 513
+    Height = 225
+    ScrollBars = ssVertical
+    TabOrder = 3
+  end
+  object btnClose: TButton
+    Left = 336
+    Top = 288
+    Width = 83
+    Height = 33
+    Caption = 'btnClose'
+    TabOrder = 4
+    OnClick = btnCloseClick
+  end
+  object MainMenu1: TMainMenu
+    Left = 496
+    Top = 65520
+    object N1: TMenuItem
+      Caption = #1060#1072#1081#1083
+      object MenuCreate: TMenuItem
+        Caption = #1057#1086#1079#1076#1072#1090#1100
+        OnClick = MenuCreateClick
+      end
+      object MenuOpen: TMenuItem
+        Caption = #1054#1090#1082#1088#1099#1090#1100
+        OnClick = MenuOpenClick
+      end
+      object MenuSave: TMenuItem
+        Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
+        OnClick = MenuSaveClick
+      end
+      object MenuSaveAs: TMenuItem
+        Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100' '#1082#1072#1082' ...'
+        OnClick = MenuSaveAsClick
+      end
+    end
+    object N2: TMenuItem
+      Caption = #1057#1086#1088#1090#1080#1088#1086#1074#1082#1072
+      object MenuSortAscending: TMenuItem
+        Caption = #1055#1086' '#1074#1086#1079#1088#1072#1089#1090#1072#1085#1080#1102
+        OnClick = MenuSortAscendingClick
+      end
+      object MenuSortDescending: TMenuItem
+        Caption = #1055#1086' '#1091#1073#1099#1074#1072#1085#1080#1102
+        OnClick = MenuSortDescendingClick
+      end
+    end
+  end
+  object OpenDialog1: TOpenDialog
+    DefaultExt = '.dat'
+    Filter = #1060#1072#1081#1083#1099' '#1076#1072#1085#1085#1099#1093'|*.dat|'#1042#1089#1077' '#1092#1072#1081#1083#1099'|*.*'
+    Left = 552
+    Top = 288
+  end
+  object SaveDialog1: TSaveDialog
+    DefaultExt = '.txt'
+    Filter = #1060#1072#1081#1083#1099' '#1076#1072#1085#1085#1099#1093'|*.dat|'#1042#1089#1077' '#1092#1072#1081#1083#1099'|*.*'
+    Left = 616
+    Top = 288
+  end
+end
---
+++ 6ПРИМ_4ВедомостьАбитуриентов/Vadim.pas
@@ -0,0 +1,385 @@
+unit Vadim;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Menus, Grids, Buttons;
+
+type
+  TStudent = record
+    name: String;
+    rating_arr: array [0..2] of Integer;
+    rating_mid: Real;
+  end;
+
+type
+  TValerievich = class(TForm)
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    Label1: TLabel;
+    GroupBox1: TGroupBox;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    edtMath: TEdit;
+    edtPhis: TEdit;
+    edtName: TEdit;
+    edtLetter: TEdit;
+    btnCreate: TButton;
+    sgrdTable: TStringGrid;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
+    MenuCreate: TMenuItem;
+    MenuOpen: TMenuItem;
+    MenuSave: TMenuItem;
+    MenuSortAscending: TMenuItem;
+    MenuSortDescending: TMenuItem;
+    MenuSaveAs: TMenuItem;
+    btnClose: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure MenuCreateClick(Sender: TObject);
+    procedure MenuOpenClick(Sender: TObject);
+    procedure btnCreateClick(Sender: TObject);
+    procedure MenuSaveClick(Sender: TObject);
+    procedure MenuSortAscendingClick(Sender: TObject);
+    procedure MenuSortDescendingClick(Sender: TObject);
+    procedure MenuSaveAsClick(Sender: TObject);
+    procedure btnCloseClick(Sender: TObject);
+
+  private
+    flag_open: Boolean;
+    flag_saved: Boolean;
+    name_cur_file: String;
+    data_file: text;
+    student_arr: array of TStudent;
+    num_record: Integer;
+    procedure WriteDataToFile();
+    procedure ReadDataFromFile();
+    function CheckCorrectName(name: String) : String;
+    function GetNameStudent() : String;
+    procedure FillString(var std: TStudent; ind: Integer);
+    procedure SwapStudents(s1, s2: Integer);
+    procedure QSort(l, r: Integer; type_sort: string);
+  public
+    { Public declarations }
+  end;
+
+var
+  Valerievich: TValerievich;
+
+implementation
+
+{$R *.dfm}
+
+//////////////////////////////
+// Private functions block  //
+//////////////////////////////
+
+procedure TValerievich.WriteDataToFile();
+var
+  i, j: Integer;
+begin
+  AssignFile(data_file, name_cur_file);
+  Rewrite(data_file);
+  for i := 0 to Length(student_arr) - 1 do begin
+    Writeln(data_file, '');
+    Writeln(data_file, student_arr[i].name);
+    for j := 0 to 1 do
+      Writeln(data_file, IntToStr(student_arr[i].rating_arr[j]));
+    Write(data_file, IntToStr(student_arr[i].rating_arr[2]));
+  end;
+  CloseFile(data_file);
+end;
+
+procedure TValerievich.ReadDataFromFile();
+var
+  tmp_str: String;
+  i: Integer;
+begin
+  AssignFile(data_file, name_cur_file);
+  Reset(data_file);
+  num_record := 0;
+  Readln(data_file, tmp_str);
+  while not EOF(data_file) do begin
+    inc(num_record);
+    SetLength(student_arr, num_record);
+    with student_arr[num_record - 1] do begin
+      Readln(data_file, name);
+      name := CheckCorrectName(name);
+      if name = '' then begin
+        ShowMessage('Неккоректный файл. Ошибка при загрузке имени');
+        exit;
+      end;
+      for i := 0 to 2 do begin
+        Readln(data_file, tmp_str);
+        try
+          rating_arr[i] := StrToInt(tmp_str);
+        except
+          ShowMessage('Неккоректный файл. Ошибка при загрузке оценок');
+          exit;
+        end;
+      end;
+    end;
+    sgrdTable.RowCount := num_record + 1;
+  end;
+  CloseFile(data_file);
+end;
+
+function TValerievich.CheckCorrectName(name: String) : String;
+var
+  i: Integer;
+begin
+  for i := 1 to Length(name) do begin
+    if not (((name[i] >='A') and (name[i] <= 'Z')) or
+        ((name[i] >='a') and (name[i] <= 'z')) or
+        (name[i] = ' ')) then
+    begin
+      name := '';
+      break;
+    end;
+  end;
+  result := name;
+end;
+
+function TValerievich.GetNameStudent() : String;
+var
+  str_tmp: String;
+begin
+  str_tmp := edtName.Text;
+  if str_tmp = '' then begin
+    ShowMessage('Поле Ф.И.О. не может быть пустым!');
+    exit;
+  end;
+  str_tmp := CheckCorrectName(str_tmp);
+  if str_tmp = '' then begin
+    ShowMessage('Поле Ф.И.О. может содержать только буквы английского алфавита!');
+    exit;
+  end;
+  result := str_tmp;
+end;
+
+procedure TValerievich.FillString(var std: TStudent; ind: Integer);
+begin
+  with std do begin
+    sgrdTable.Cells[0, ind] := name;
+    sgrdTable.Cells[1, ind] := IntToStr(rating_arr[0]);
+    sgrdTable.Cells[2, ind] := IntToStr(rating_arr[1]);
+    sgrdTable.Cells[3, ind] := IntToStr(rating_arr[2]);
+    rating_mid := (rating_arr[0] + rating_arr[1] + rating_arr[2]) / 3;
+    sgrdTable.Cells[4, ind] := FloatToStrF(rating_mid, fffixed, 3, 3);
+  end;
+end;
+
+procedure TValerievich.SwapStudents(s1, s2: Integer);
+var
+  tmp_str: String;
+  tmp_int, k: Integer;
+  tmp_real: Real;
+begin
+  tmp_str := student_arr[s1].name;
+  tmp_real := student_arr[s1].rating_mid;
+  student_arr[s1].name := student_arr[s2].name;
+  student_arr[s1].rating_mid := student_arr[s2].rating_mid;
+  student_arr[s2].name := tmp_str;
+  student_arr[s2].rating_mid := tmp_real;
+  for k := 0 to 2 do begin
+    tmp_int := student_arr[s1].rating_arr[k];
+    student_arr[s1].rating_arr[k] := student_arr[s2].rating_arr[k];
+    student_arr[s2].rating_arr[k] := tmp_int;
+  end;
+end;
+
+procedure TValerievich.QSort(l, r: Integer; type_sort: string);
+var
+  i, j: Integer;
+  q: Real;
+begin
+  i := l;
+  j := r;
+  q := student_arr[(l + r) div 2].rating_mid;
+  repeat
+    if type_sort = 'Ascending' then begin
+      while (student_arr[i].rating_mid < q) do i := i + 1;
+      while (q < student_arr[j].rating_mid) do j := j - 1;
+    end else if type_sort = 'Descending' then begin
+      while (student_arr[i].rating_mid > q) do i := i + 1;
+      while (q > student_arr[j].rating_mid) do j := j - 1;
+    end;
+    if (i <= j) then begin
+      SwapStudents(i, j);
+      j := j - 1;
+      i := i + 1;
+    end;
+  until (i > j);
+  if (l < j) then QSort(l, j, type_sort);
+  if (i < r) then QSort(i, r, type_sort);
+end;
+
+//////////////////////////////
+// Form callbacks block     //
+//////////////////////////////
+
+procedure TValerievich.FormCreate(Sender: TObject);
+var
+  i: Integer;
+begin
+  edtName.Text := 'Akimov Vadim';
+  edtMath.Text := '2';
+  edtPhis.Text := '3';
+  edtLetter.Text := '2';
+
+  btnCreate.Caption := 'Создать новую запись';
+
+  sgrdTable.FixedCols := 0;
+  sgrdTable.FixedRows := 1;
+  for i := 0 to sgrdTable.ColCount - 1 do
+    sgrdTable.ColWidths[i] := sgrdTable.Width div sgrdTable.ColCount;
+    
+  with sgrdTable do begin
+    Cells[0, 0] := 'Ф.И.О.';
+    Cells[1, 0] := 'Матем.';
+    Cells[2, 0] := 'Физика';
+    Cells[3, 0] := 'Сочинение';
+    Cells[4, 0] := 'Сред. балл';
+  end;
+  
+  btnClose.Caption := 'Close';
+
+  flag_open := True;
+  flag_saved := True;
+
+  SetLength(student_arr, 0);
+
+  name_cur_file := 'DataFiles\group23a2.dat';
+  ReadDataFromFile();
+  for i := 1 to Length(student_arr) do
+    FillString(student_arr[i - 1], i);
+end;
+
+procedure TValerievich.MenuCreateClick(Sender: TObject);
+var
+  i, j: Integer;
+begin
+  OpenDialog1.Title := 'Создать новый файл';
+  if OpenDialog1.Execute then begin
+    flag_open := True;
+    name_cur_file := OpenDialog1.FileName;
+    AssignFile(data_file, OpenDialog1.FileName);
+    Rewrite(data_file);
+    btnCreate.Show;
+    num_record := 0;
+    for i := 1 to num_record do
+      for j := 0 to 5 do
+        sgrdTable.Cells[j, i] := '';
+    flag_open := True;
+    CloseFile(data_file);
+  end;
+end;
+
+procedure TValerievich.MenuOpenClick(Sender: TObject);
+var
+  i: Integer;
+begin
+  OpenDialog1.Title := 'Открыть файл';
+  if OpenDialog1.Execute then begin
+    flag_open := True;
+    name_cur_file := OpenDialog1.FileName;
+    ReadDataFromFile();
+    for i := 1 to Length(student_arr) do
+      FillString(student_arr[i - 1], i);
+    btnCreate.Show;
+  end;
+end;
+
+procedure TValerievich.btnCreateClick(Sender: TObject);
+var
+  i: Integer;
+begin
+  flag_saved := False;
+  inc(num_record);
+  sgrdTable.RowCount := num_record + 1;
+  SetLength(student_arr, num_record);
+  Label1.Caption := IntToStr(num_record);
+  with student_arr[num_record - 1] do begin
+    name := GetNameStudent();
+    if name = '' then begin
+      exit;
+    end;
+    try
+      rating_arr[0] := StrToInt(edtMath.Text);
+    except
+      ShowMessage('Некорректное значение в поле Математика');
+      exit;
+    end;
+    try
+      rating_arr[1] := StrToInt(edtPhis.Text);
+    except
+      ShowMessage('Некорректное значение в поле Физика');
+      exit;
+    end;
+    try
+      rating_arr[2] := StrToInt(edtLetter.Text);
+    except
+      ShowMessage('Некорректное значение в поле Сочинение');
+      exit;
+    end;
+  end;
+  FillString(student_arr[num_record - 1], num_record);
+end;
+
+procedure TValerievich.MenuSaveClick(Sender: TObject);
+var
+  i, j: Integer;
+begin
+  flag_saved := True;
+  if flag_open then begin
+    WriteDataToFile();
+    flag_open := False;
+  end;
+end;
+
+procedure TValerievich.MenuSortAscendingClick(Sender: TObject);
+var
+  i: Integer;
+begin
+  flag_saved := False;
+  QSort(0, Length(student_arr) - 1, 'Ascending');
+  for i := 1 to Length(student_arr) do
+    FillString(student_arr[i - 1], i);
+end;
+
+procedure TValerievich.MenuSortDescendingClick(Sender: TObject);
+var
+  i: Integer;
+begin
+  flag_saved := False;
+  QSort(0, Length(student_arr) - 1, 'Descending');
+  for i := 1 to Length(student_arr) do
+    FillString(student_arr[i - 1], i);
+end;
+
+procedure TValerievich.MenuSaveAsClick(Sender: TObject);
+var
+  i, j: Integer;
+begin
+  flag_saved := True;
+
+  SaveDialog1.Title := 'Сохранить как ...';
+  if SaveDialog1.Execute then begin
+    name_cur_file := SaveDialog1.FileName;
+    WriteDataToFile();
+  end;
+end;
+
+procedure TValerievich.btnCloseClick(Sender: TObject);
+begin
+  if flag_open and (not flag_saved) then
+    WriteDataToFile();
+  Close;
+end;
+
+end.
+
+
